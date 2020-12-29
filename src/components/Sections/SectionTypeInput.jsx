56efe14d37d88412287ef53a106ca407eff932c1
jnk-keynoty <jnk@keynoty.com>
2020-12-29 13:52:09 +0900

    minor change4

--- src/components/Sections/SectionTypeInput.jsx
+++ src/components/Sections/SectionTypeInput.jsx
@@ -58,8 +58,9 @@ function SectionTypeInput(props) {
             method: 'post',
             withCredentials: false,
             headers: {
-              'Access-Control-Allow-Headers': '*',
+              'Access-Control-Allow-Headers': "Origin, X-Requested-With, Content-Type, Accept, Authorization" ,
               'Access-Control-Allow-Origin': '*',
+              "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
             },
             url: 'https://strapi.almightydr.com/inquiries',
             data: values,
